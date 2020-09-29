f04905a6b35569041017f18b7385948c3642947c
Luke O'Malley <Lukeomalley95@gmail.com>
2020-09-29 10:11:43 -0700

    added variable declarations to ast generator

--- src/main/java/com/lukeomalley/lox/Stmt.java
+++ src/main/java/com/lukeomalley/lox/Stmt.java
@@ -1,10 +1,12 @@
 package com.lukeomalley.lox;
 
+import java.util.List;
+
 abstract class Stmt {
   interface Visitor<R> {
     R visitExpressionStmt(Expression stmt);
-
     R visitPrintStmt(Print stmt);
+    R visitVarStmt(Var stmt);
   }
 
   static class Expression extends Stmt {
@@ -32,6 +34,22 @@ abstract class Stmt {
       return visitor.visitPrintStmt(this);
     }
   }
+
+  static class Var extends Stmt {
+    final Token name;
+    final Expr initializer;
+
+    Var(Token name, Expr initializer) {
+      this.name = name;
+      this.initializer = initializer;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitVarStmt(this);
+    }
+  }
+
 
   abstract <R> R accept(Visitor<R> visitor);
 }
