39448f1d449ba307b2af158c8179e20d6a0679c9
Luke O'Malley <Lukeomalley95@gmail.com>
2020-09-29 08:13:57 -0700

    added support for expression and print statements

--- src/main/java/com/lukeomalley/lox/Interpreter.java
+++ src/main/java/com/lukeomalley/lox/Interpreter.java
@@ -1,11 +1,15 @@
 package com.lukeomalley.lox;
 
-class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
 
-  void interpret(Expr expression) {
+class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+
+  void interpret(List<Stmt> statements) {
     try {
-      Object value = evaluate(expression);
-      System.out.println("=> " + stringify(value));
+      for (Stmt statement : statements) {
+        execute(statement);
+
+      }
     } catch (RuntimeError error) {
       Lox.runtimeError(error);
     }
@@ -26,6 +30,19 @@ class Interpreter implements Expr.Visitor<Object> {
     }
 
     return object.toString();
+  }
+
+  @Override
+  public Void visitExpressionStmt(Stmt.Expression stmt) {
+    evaluate(stmt.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitPrintStmt(Stmt.Print stmt) {
+    Object value = evaluate(stmt.expression);
+    System.out.println(stringify(value));
+    return null;
   }
 
   @Override
@@ -101,6 +118,10 @@ class Interpreter implements Expr.Visitor<Object> {
 
   private Object evaluate(Expr expr) {
     return expr.accept(this);
+  }
+
+  private void execute(Stmt stmt) {
+    stmt.accept(this);
   }
 
   private boolean isTruthy(Object object) {
