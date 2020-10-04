6778873f701f15cbb4b215eba528f80ff00860b8
Luke O'Malley <Lukeomalley95@gmail.com>
2020-10-04 12:32:54 -0700

    implemented evaluating conditionals

--- src/main/java/com/lukeomalley/lox/Parser.java
+++ src/main/java/com/lukeomalley/lox/Parser.java
@@ -50,11 +50,29 @@ public class Parser {
       return printStatement();
     }
 
+    if (match(TokenType.IF)) {
+      return ifStatement();
+    }
+
     if (match(TokenType.LEFT_BRACE)) {
       return new Stmt.Block(block());
     }
 
     return expressionStatement();
+  }
+
+  private Stmt ifStatement() {
+    consume(TokenType.LEFT_PAREN, "Expect '(' after 'if'.");
+    Expr condition = expression();
+    consume(TokenType.RIGHT_PAREN, "Expect ')' after if condition.");
+
+    Stmt thenBranch = statement();
+    Stmt elseBranch = null;
+    if (match(TokenType.ELSE)) {
+      elseBranch = statement();
+    }
+
+    return new Stmt.If(condition, thenBranch, elseBranch);
   }
 
   private List<Stmt> block() {
--- src/main/java/com/lukeomalley/tool/GenerateAst.java
+++ src/main/java/com/lukeomalley/tool/GenerateAst.java
@@ -17,10 +17,9 @@ public class GenerateAst {
     }
 
     String outputDir = args[0];
-    defineAst(outputDir, "Expr",
-        Arrays.asList("Binary : Expr left, Token operator, Expr right", "Grouping : Expr expression",
-            "Literal : Object value", "Unary : Token operator, Expr right", "Variable : Token name",
-            "Assign : Token name, Expr value"));
+    defineAst(outputDir, "Expr", Arrays.asList("Binary : Expr left, Token operator, Expr right",
+        "Grouping : Expr expression", "Literal : Object value", "Unary : Token operator, Expr right",
+        "Logical : Expr left, Token operator, Expr right", "Variable : Token name", "Assign : Token name, Expr value"));
 
     defineAst(outputDir, "Stmt",
         Arrays.asList("Expression : Expr expression", "Print : Expr expression", "Var : Token name, Expr initializer",
