6c0e175176d3fe9ce59e8460b7da009b451e2853
Luke O'Malley <Lukeomalley95@gmail.com>
2020-11-09 10:00:18 -0500

    started implementation of resolving pass

---
+++ src/main/java/com/lukeomalley/lox/Resolver.java
@@ -0,0 +1,79 @@
+package com.lukeomalley.lox;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+import com.lukeomalley.lox.Stmt.Block;
+import com.lukeomalley.lox.Stmt.Var;
+
+class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
+  private final Interpreter interpreter;
+  private final Stack<Map<String, Boolean>> scopes = new Stack<>();
+
+  Resolver(Interpreter interpreter) {
+    this.interpreter = interpreter;
+  }
+
+  void resolve(List<Stmt> statements) {
+    for (Stmt statement : statements) {
+      resolve(statement);
+    }
+  }
+
+  private void resolve(Stmt stmt) {
+    stmt.accept(this);
+  }
+
+  private void resolve(Expr expr) {
+    expr.accept(this);
+  }
+
+  @Override
+  public Void visitBlockStmt(Block stmt) {
+    beginScope();
+    resolve(stmt.statements);
+    endScope();
+    return null;
+  }
+
+  @Override
+  public Void visitVarStmt(Var stmt) {
+    declare(stmt.name);
+    if (stmt.initializer != null) {
+      resolve(stmt.initializer);
+    }
+
+    define(stmt.name);
+
+    return null;
+  }
+
+  private void declare(Token name) {
+    if (scopes.isEmpty()) {
+      return;
+    }
+
+    Map<String, Boolean> scope = scopes.peek();
+
+    scope.put(name.lexeme, false);
+  }
+
+  private void define(Token name) {
+    if (scopes.isEmpty()) {
+      return;
+    }
+
+    scopes.peek().put(name.lexeme, true);
+  }
+
+  private void beginScope() {
+    scopes.push(new HashMap<String, Boolean>());
+  }
+
+  private void endScope() {
+    scopes.pop();
+  }
+
+}
