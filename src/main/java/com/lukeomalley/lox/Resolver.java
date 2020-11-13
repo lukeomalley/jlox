a3145432d4d6f0af28a83679df2b737dc1d30906
Luke O'Malley <Lukeomalley95@gmail.com>
2020-11-13 11:04:53 -0500

    implemented variable resolving pass

--- src/main/java/com/lukeomalley/lox/Resolver.java
+++ src/main/java/com/lukeomalley/lox/Resolver.java
@@ -5,8 +5,22 @@ import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
+import com.lukeomalley.lox.Expr.Assign;
+import com.lukeomalley.lox.Expr.Binary;
+import com.lukeomalley.lox.Expr.Call;
+import com.lukeomalley.lox.Expr.Grouping;
+import com.lukeomalley.lox.Expr.Literal;
+import com.lukeomalley.lox.Expr.Logical;
+import com.lukeomalley.lox.Expr.Unary;
+import com.lukeomalley.lox.Expr.Variable;
 import com.lukeomalley.lox.Stmt.Block;
+import com.lukeomalley.lox.Stmt.Expression;
+import com.lukeomalley.lox.Stmt.Function;
+import com.lukeomalley.lox.Stmt.If;
+import com.lukeomalley.lox.Stmt.Print;
+import com.lukeomalley.lox.Stmt.Return;
 import com.lukeomalley.lox.Stmt.Var;
+import com.lukeomalley.lox.Stmt.While;
 
 class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   private final Interpreter interpreter;
@@ -48,6 +62,136 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     define(stmt.name);
 
     return null;
+  }
+
+  @Override
+  public Void visitVariableExpr(Variable expr) {
+    if (!scopes.isEmpty() && scopes.peek().get(expr.name.lexeme) == Boolean.FALSE) {
+      Lox.error(expr.name, "Can't read local varible in its own initializer.");
+    }
+
+    resolveLocal(expr, expr.name);
+    return null;
+  }
+
+  @Override
+  public Void visitAssignExpr(Assign expr) {
+    resolve(expr.value);
+    resolveLocal(expr, expr.name);
+    return null;
+  }
+
+  @Override
+  public Void visitFunctionStmt(Function stmt) {
+    declare(stmt.name);
+    define(stmt.name);
+
+    resolveFunction(stmt);
+    return null;
+  }
+
+  private void resolveFunction(Function function) {
+    beginScope();
+
+    for (Token param : function.params) {
+      declare(param);
+      define(param);
+    }
+    resolve(function.body);
+
+    endScope();
+  }
+
+  @Override
+  public Void visitExpressionStmt(Expression stmt) {
+    resolve(stmt.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitIfStmt(If stmt) {
+    resolve(stmt.condition);
+    resolve(stmt.thenBranch);
+
+    if (stmt.elseBranch != null) {
+      resolve(stmt.elseBranch);
+    }
+
+    return null;
+  }
+
+  @Override
+  public Void visitPrintStmt(Print stmt) {
+    resolve(stmt.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitReturnStmt(Return stmt) {
+    if (stmt.value != null) {
+      resolve(stmt.value);
+    }
+
+    return null;
+  }
+
+  @Override
+  public Void visitWhileStmt(While stmt) {
+    resolve(stmt.condition);
+    resolve(stmt.body);
+    return null;
+  }
+
+  @Override
+  public Void visitBinaryExpr(Binary expr) {
+    resolve(expr.left);
+    resolve(expr.right);
+    return null;
+  }
+
+  @Override
+  public Void visitCallExpr(Call expr) {
+    resolve(expr.callee);
+
+    for (Expr argument : expr.arguments) {
+      resolve(argument);
+    }
+
+    return null;
+  }
+
+  @Override
+  public Void visitGroupingExpr(Grouping expr) {
+    resolve(expr.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitLiteralExpr(Literal expr) {
+    return null;
+  }
+
+  @Override
+  public Void visitLogicalExpr(Logical expr) {
+    resolve(expr.left);
+    resolve(expr.right);
+    return null;
+  }
+
+  @Override
+  public Void visitUnaryExpr(Unary expr) {
+    resolve(expr.right);
+    return null;
+  }
+
+  private void resolveLocal(Expr expr, Token name) {
+    for (int i = scopes.size() - 1; i >= 0; i--) {
+      if (scopes.get(i).containsKey(name.lexeme)) {
+        interpreter.resolve(expr, scopes.size() - 1 - i);
+        return;
+      }
+
+    }
   }
 
   private void declare(Token name) {
