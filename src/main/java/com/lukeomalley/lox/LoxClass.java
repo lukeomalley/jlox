9ca30c4dd5cfa1451a4e094b50a72dd7a99389c4
Luke O'Malley <Lukeomalley95@gmail.com>
2020-11-14 18:43:09 -0500

    implemented basic inheritance and added super expression node to ast

--- src/main/java/com/lukeomalley/lox/LoxClass.java
+++ src/main/java/com/lukeomalley/lox/LoxClass.java
@@ -6,15 +6,21 @@ import java.util.Map;
 public class LoxClass implements LoxCallable {
   final String name;
   private final Map<String, LoxFunction> methods;
+  final LoxClass superclass;
 
-  LoxClass(String name, Map<String, LoxFunction> methods) {
+  LoxClass(String name, LoxClass superclass, Map<String, LoxFunction> methods) {
     this.name = name;
     this.methods = methods;
+    this.superclass = superclass;
   }
 
   public LoxFunction findMethod(String name) {
     if (this.methods.containsKey(name)) {
       return this.methods.get(name);
+    }
+
+    if (superclass != null) {
+      return superclass.findMethod(name);
     }
 
     return null;
--- src/main/java/com/lukeomalley/tool/GenerateAst.java
+++ src/main/java/com/lukeomalley/tool/GenerateAst.java
@@ -13,12 +13,11 @@ public class GenerateAst {
     }
 
     String outputDir = args[0];
-    defineAst(outputDir, "Expr",
-        Arrays.asList("Binary : Expr left, Token operator, Expr right", "Grouping : Expr expression",
-            "Literal : Object value", "Unary : Token operator, Expr right",
-            "Call : Expr callee, Token paren, List<Expr> arguments", "Logical : Expr left, Token operator, Expr right",
-            "Variable : Token name", "Assign : Token name, Expr value", "Get : Expr object, Token name",
-            "Set : Expr object, Token name, Expr value", "This : Token keyword"));
+    defineAst(outputDir, "Expr", Arrays.asList("Binary : Expr left, Token operator, Expr right",
+        "Grouping : Expr expression", "Literal : Object value", "Unary : Token operator, Expr right",
+        "Call : Expr callee, Token paren, List<Expr> arguments", "Logical : Expr left, Token operator, Expr right",
+        "Variable : Token name", "Assign : Token name, Expr value", "Get : Expr object, Token name",
+        "Set : Expr object, Token name, Expr value", "This : Token keyword", "Super : Token keyword, Token method"));
 
     defineAst(outputDir, "Stmt",
         Arrays.asList("Expression : Expr expression", "Print : Expr expression", "Var : Token name, Expr initializer",
