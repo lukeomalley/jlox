9a94b99e5db63726f0f7ce4af61756863f0e93cf
Luke O'Malley <Lukeomalley95@gmail.com>
2020-11-14 18:08:13 -0500

    prevent init from returning a value and always return the class instance being initialized

--- src/main/java/com/lukeomalley/lox/Resolver.java
+++ src/main/java/com/lukeomalley/lox/Resolver.java
@@ -37,7 +37,7 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
   }
 
   private enum FunctionType {
-    NONE, FUNCTION, METHOD
+    NONE, FUNCTION, METHOD, INITIALIZER
   }
 
   private enum ClassType {
@@ -152,6 +152,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     }
 
     if (stmt.value != null) {
+      if (currentFunction == FunctionType.INITIALIZER) {
+        Lox.error(stmt.keyword, "Can't return a value from an initializer");
+      }
+
       resolve(stmt.value);
     }
 
@@ -220,6 +224,10 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     for (Stmt.Function method : stmt.methods) {
       FunctionType declaration = FunctionType.METHOD;
+      if (method.name.lexeme.equals("init")) {
+        declaration = FunctionType.INITIALIZER;
+      }
+
       resolveFunction(method, declaration);
     }
 
